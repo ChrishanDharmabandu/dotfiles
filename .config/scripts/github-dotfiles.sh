b7e6fbe6e71751bdeb2b4f9eb81a620d5afc30df
Chrishan Dharmabandu <dchrishan90@gmail.com>
2024-02-29 10:52:34 +1100

    updated script for pushing dotfiles

---
+++ .config/scripts/github-dotfiles.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+# Check the current remote URL
+config remote -v
+
+# Change the remote URL to SSH
+config remote set-url origin git@github.com:chrishandharmabandu/dotfiles.git
+
+# Verify the updated remote URL
+config remote -v
+
+# Push changes with upstream branch
+config push --set-upstream origin master
+
