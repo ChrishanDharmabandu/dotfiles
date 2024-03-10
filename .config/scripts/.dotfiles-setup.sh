0bcd84605d29c82120003b5815f3e3a7fe8bbfa6
Chrishan Dharmabandu <dchrishan90@gmail.com>
2024-03-10 15:45:15 +1100

    uppdated scripts

---
+++ .config/scripts/.dotfiles-setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+git clone --bare https://github.com/chrishandharmabandu/dotfiles.git $HOME/.dotfiles
+
+function config {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+mkdir -p .dotfiles-backup
+config checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles from git@github.com:chrishandharmabandu/dotfiles.git";
+else
+  echo "Moving existing dotfiles to ~/.dotfiles-backup";
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi
+
+config checkout --force
+config config status.showUntrackedFiles no
+
+# Script to clone git repos
+# Specify the paths and URLs
+zsh_plug_dir="$HOME/.zsh"
+fast_syntax_highlighting_url="https://github.com/zdharma-continuum/fast-syntax-highlighting.git"
+fzf_tab_url="https://github.com/Aloxaf/fzf-tab.git"
+zsh_autosuggestions_url="https://github.com/zsh-users/zsh-autosuggestions.git"
+
+# Remove existing directories
+rm -rf "${zsh_plug_dir}/fast-syntax-highlighting"
+rm -rf "${zsh_plug_dir}/fzf-tab"
+rm -rf "${zsh_plug_dir}/zsh-autosuggestions"
+
+# Clone the repositories
+git clone "${fast_syntax_highlighting_url}" "${zsh_plug_dir}/fast-syntax-highlighting"
+git clone "${fzf_tab_url}" "${zsh_plug_dir}/fzf-tab"
+git clone "${zsh_autosuggestions_url}" "${zsh_plug_dir}/zsh-autosuggestions"
+
