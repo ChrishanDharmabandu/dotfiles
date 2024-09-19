671f65d36293e29dc51fb288b0d450cb73db6009
Chrishan Dharmabandu <dchrishan90@gmail.com>
2024-09-19 18:24:15 +1000

    updated init.lua for nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -897,17 +897,38 @@ require("lazy").setup({
 	-- 		-- You can configure highlights by doing something like:
 	-- 		vim.cmd.hi("Comment gui=none")
 	-- 	end,
+	-- -- },
+	-- { -- custom colorscheme
+	-- 	"catppuccin/nvim",
+	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
+	-- 	init = function()
+	-- 		-- Load the colorscheme here.
+	-- 		-- Like many other themes, this one has different styles, and you could load
+	-- 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+	-- 		vim.cmd.colorscheme("catppuccin-mocha")
+	-- 		-- You can configure highlights by doing something like:
+	-- 		vim.cmd.hi("Comment gui=none")
+	-- 	end,
 	-- },
 	{ -- custom colorscheme
-		"catppuccin/nvim",
+		"rebelot/kanagawa.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("catppuccin-mocha")
-			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+	{
+		"xiyaowong/transparent.nvim",
+		priority = 1000, -- Ensure it loads early
+		config = function()
+			require("transparent").setup({
+				extra_groups = {
+					"NormalFloat", -- For floating panels (Lazy, Mason, LspInfo)
+					"NvimTreeNormal", -- For NvimTree
+				},
+			})
+			-- Enable transparency on startup
+			vim.cmd("TransparentEnable")
 		end,
 	},
 
