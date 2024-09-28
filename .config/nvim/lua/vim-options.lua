524447ea644f615119181f4bea3f4c92bedb5908
Chrishan Dharmabandu <dchrishan90@gmail.com>
2024-09-29 09:33:27 +1000

    auto-update

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -47,9 +47,6 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
--- Unbind space in visual
--- vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
