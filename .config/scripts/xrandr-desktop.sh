410b5bb777703442fa669c55c0374f26e86a4583
ChrishanDharmabandu <51555126+ChrishanDharmabandu@users.noreply.github.com>
2025-06-05 04:36:33 +1000

    Update xrandr-desktop.sh

--- .config/scripts/xrandr-desktop.sh
+++ .config/scripts/xrandr-desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output DP-0 --mode 1920x1080 --rate 239.76 --primary --output HDMI-0 --mode 1920x1080 --rate 60 --same-as DP-0
+# xrandr --output DP-0 --mode 1920x1080 --rate 239.76 --primary --output HDMI-0 --mode 1920x1080 --rate 60 --same-as DP-0
